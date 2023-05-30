b3a5c7c894ae1ca79c7f4f47498dea1b4694accc
Jesus Sanchez <jesus.sanchez@nearshoretechnology.com>
2023-05-29 19:51:21 -0600

    Add dependependencies to the pom.xml and Add URLs to Routes.java

--- RestAssured-API-Automation/src/main/java/api/endpoints/Routes.java
+++ RestAssured-API-Automation/src/main/java/api/endpoints/Routes.java
@@ -1,4 +1,38 @@
 package api.endpoints;
 
 public class Routes {
+
+    /**
+     * This class contains endpoint URLs
+     **/
+    //BaseUrl
+    public static String baseUrl = "https://simple-tool-rental-api.glitch.me";
+
+    //Get Status
+    public static String getStatus = baseUrl + "/status";
+
+    //Get all tools
+    public static String getAllTools = baseUrl + "/tools";
+
+    //Get a single tool
+    public static String getSingleTool = baseUrl + "/tools/:toolId";
+
+    //Get orders
+    public static String getOrders = baseUrl + "/orders";
+
+    //Get single order
+    public static String getSingleOrder = baseUrl + "/orders/:orderId";
+
+    //Create a new order
+    public static String createNewOrder = baseUrl + "/orders";
+
+    //Update an order
+    public static String updateOrder = baseUrl + "/orders/:orderId";
+
+    //Delete an order
+    public static String deleteOrder = baseUrl + "/orders/:orderId";
+
+    //Register a new API client(Get access token)
+    public static String clientRegister = baseUrl + "/api-clients";
+
 }
---
+++ RestAssured-API-Automation/src/main/java/api/pojos/Authentication.java
@@ -0,0 +1,2 @@
+package api.pojos;public class Authentication {
+}
