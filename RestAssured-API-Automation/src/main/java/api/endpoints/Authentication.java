5e895c53a609cd8f83aca372f056e5148843a3a8
Jesus Sanchez <jesus.sanchez@nearshoretechnology.com>
2023-06-01 12:12:26 -0600

    Add post request to register client and get accessToken

--- RestAssured-API-Automation/src/main/java/api/endpoints/Authentication.java
+++ RestAssured-API-Automation/src/main/java/api/endpoints/Authentication.java
@@ -1,12 +1,28 @@
 package api.endpoints;
 
+import api.pojos.ClientAuthentication;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.http.ContentType.JSON;
 
 public class Authentication {
+    ClientAuthentication client = new ClientAuthentication();
 
+    //Create client and get accessToken
     public Response registerClient(){
-        Response tokenClient =null;
+        client.setClientEmail("some@email.com");
+        client.setClientName("Some Name");
+        Response tokenClient = RestAssured
+            .given()
+                .contentType(JSON)
+                .body(client)
+            .when()
+                .post(Routes.clientRegister);
 
+        System.out.println(tokenClient.then().log().all());
         return tokenClient;
 
     }
